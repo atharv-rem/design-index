2a3938f5fb5554abbb698c18a0f0ace69de6ea5d
Atharv Remeshan <atharv.rem@gmail.com>
2025-06-13 21:45:14 +0530

    left bar complete

--- design-directory/src/App.jsx
+++ design-directory/src/App.jsx
@@ -4,6 +4,9 @@ import icon from "./assets/home-01-stroke-rounded.svg"
 import mockup from "./assets/hold-phone-stroke-rounded.svg"
 import design_inspo from "./assets/canvas-stroke-rounded.svg"
 import illustration from "./assets/ai-beautify-stroke-rounded.svg"
+import send from "./assets/sent-stroke-rounded.svg"
+import linkedin from "./assets/linkedin-02-stroke-rounded.svg"
+import twitter from "./assets/new-twitter-stroke-rounded.svg"
 
 export default function App() {
   return (
@@ -12,7 +15,7 @@ export default function App() {
       <div className="flex w-full h-screen p-[10px] overflow-hidden"> 
 
         {/* Left sidebar for navigation*/}
-        <div className="flex-row items-center justify-center w-1/5 bg-white h-screen position-fixed top-0 left-0">
+        <div className="flex-col items-center justify-center w-1/5 bg-white h-screen position-fixed top-0 left-0">
           
           <div className="flex flex-col items-start justify-center h-auto gap-0 leading-none pt-2 font-medium font-SG w-full">
             <span className="text-[22px] pl-2 ">design</span>
@@ -20,7 +23,8 @@ export default function App() {
           </div>
 
           {/* Navigation bar */}
-          <div className="flex flex-col items-start justify-start w-full h-[230px] mt-4 mr-2 overflow-y-auto hide-scrollbar">
+          <div className="flex flex-col items-start justify-start w-full h-[230px] mt-2 mr-2 overflow-y-auto hide-scrollbar">
+              
               <div className="flex flex-row items-center justify-start w-full h-auto">
               <img src={color} alt="color icon" className="ml-[10px] w-[30px] h-[30px] p-[5px] border-[1.5px] border-[#EBEBEB] rounded-[10px]" />
               <span className="ml-[10px] items-start justify-center font-Afacad font-medium text-[30px]">colours</span>
@@ -52,11 +56,31 @@ export default function App() {
               </div>
 
           </div> 
+
+          {/*advertisement area*/}
+          <div className="border-[2px] border-[#F6F6F6] rounded-[10px] w-[200px] h-[190px] ml-2 mr-4 flex items-center justify-center">
+          </div>
+
+          {/* submit a tool button */}
+          <button className="w-[200px] h-auto hover:border-[#e4e4e4] bg-white border-[2px] border-[#F6F6F6] rounded-[20px] p-[2px] ml-2 mr-4 mt-2 flex flex-row items-center justify-center opacity-70 hover:opacity-100 hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out">
+            <img src={send} alt="submit icon" className="w-[30px] h-[30px] p-[5px] mr-[2px]" />
+            <span className="text-[20px] font-Afacad font-bold">submit a tool</span>
+          </button>
+
+          {/* footer with social media links */}
+          <div className=" flex flex-row items-center justify-center w-[200px] ml-2 mr-4 h-[20px] mt-3 bg-white ">
+            <div className="text-[12px] font-Afacad font-bold text-black ">made by Atharv Remeshan</div>
+            <img src={linkedin} alt="linkedin icon" className="w-[12px] h-[12px] mx-[5px] hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out" />
+            <img src={twitter} alt="twitter icon" className="w-[12px] h-[12px]mx-[5px] hover:cursor-pointer hover:translate-y-[-2px] transition-all duration-200 ease-in-out" />          
+          </div>
+          
         </div>
 
-        {/* left content area*/}
+
+        {/* right content area*/}
         <div className="flex-row items-center justify-center w-4/5 p-4 bg-white overflow-y-scroll hide-scrollbar border-[2px] border-[#F6F6F6] rounded-[10px]">
         </div>
+        
         
       </div>  
     </>
