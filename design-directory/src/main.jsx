7b385ed02a9ee21667499fc22a521d220c0e155c
Atharv Remeshan <atharv.rem@gmail.com>
2025-07-25 21:37:31 +0530

    implemented GA

--- design-directory/src/main.jsx
+++ design-directory/src/main.jsx
@@ -10,6 +10,21 @@ const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
 }
+const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+
+if (GA_ID && location.hostname !== 'localhost') {
+  // Load GA script dynamically
+  const script = document.createElement('script');
+  script.setAttribute('async', '');
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ window.dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', GA_ID);
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
