b4e2232e85cd81a47f99fd47b11720747545d07a
Atharv Remeshan <atharv.rem@gmail.com>
2025-08-24 18:50:40 +0530

    posthog unused bundling resolved

--- design-directory/src/main.jsx
+++ design-directory/src/main.jsx
@@ -3,35 +3,56 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
-import {HeadProvider} from 'react-head'
+import { HeadProvider } from 'react-head'
 import { PostHogProvider } from 'posthog-js/react'
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: '2025-05-24',
-}
-const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID;
+  capture_pageview: true,
+  capture_pageleave: true,
+  capture_pageview_autocapture: true,
 
+  session_recording: {
+    maskAllInputs: false, // or true if you want sensitive data hidden
+    recordCanvas: true,
+  },
+  disable_surveys: true,
+  disable_toolbar: true,
+  disable_feature_flags: true,
+  disable_compression: true,
+  disable_session_recording: false, // keep recordings enabled
+}
+
+const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID
+const POSTHOG_KEY = import.meta.env.VITE_PUBLIC_POSTHOG_KEY
+
+// --- Lazy load GA ---
 if (GA_ID && location.hostname !== 'localhost') {
   window.addEventListener('load', () => {
-    const script = document.createElement('script');
-    script.setAttribute('async', '');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-    document.head.appendChild(script);
+    import('posthog-js').then((posthog) => {
+      if (POSTHOG_KEY) {
+        posthog.init(POSTHOG_KEY, options)
+      }
+    })
 
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){ window.dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', GA_ID);
-  });
+    const script = document.createElement('script')
+    script.async = true
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+    document.head.appendChild(script)
+
+    window.dataLayer = window.dataLayer || []
+    function gtag(){ window.dataLayer.push(arguments) }
+    gtag('js', new Date())
+    gtag('config', GA_ID)
+  })
 }
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
-      <HeadProvider>{/*helps to add meta tags to each page*/}
-        <Router> {/*helps to navigate by making changes in the URL*/}
+    {/* PostHogProvider still wraps App so hooks/components work */}
+    <PostHogProvider apiKey={POSTHOG_KEY} options={options}>
+      <HeadProvider>
+        <Router>
           <App />
         </Router>
       </HeadProvider>
